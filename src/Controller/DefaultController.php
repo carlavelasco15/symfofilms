c72eaf5f477d491f7620a612559e541c9cb97201
Carla Velasco <carlavelasco15@gmail.com>
2022-06-11 10:00:14 +0200

    send emails using template (contact)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,8 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PeliculaRepository;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use App\Form\ContactFormType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mime\Address;
 
 class DefaultController extends AbstractController
 {
@@ -21,6 +27,43 @@ class DefaultController extends AbstractController
 
         return $this->render('portada.html.twig', [
             'peliculas' => $peliculas
+        ]);
+    }
+
+
+      /**
+    * @Route("/contact", name="contacto")
+    */
+    public function contacto(Request $request, MailerInterface $mailer): Response
+    {
+
+        $formulario = $this->createForm(ContactFormType::class);
+        $formulario->handleRequest($request);
+
+        if($formulario->isSubmitted() && $formulario->isValid()) {
+
+            $datos = $formulario->getData();
+            
+            $email = new TemplatedEmail();
+            $email->from(new Address($datos['email'], $datos['nombre']))
+                ->to($this->getParameter('app.admin_email'))
+                ->subject($datos['asunto'])
+                ->htmlTemplate('email/contact.html.twig')
+                ->context([
+                    'de' => $datos['email'],
+                    'nombre' => $datos['nombre'],
+                    'asunto' => $datos['asunto'],
+                    'mensaje' => $datos['mensaje']
+                ]);
+            
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Mensaje enviado correctamente.');
+            return $this->redirectToRoute('portada');
+        }
+
+        return $this->render('contacto.html.twig', [
+            "formulario" => $formulario->createView()
         ]);
     }
 }
